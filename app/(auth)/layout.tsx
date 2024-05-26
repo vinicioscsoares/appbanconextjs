35527b03ce105505c6497e887d5bc000537c55a2
Vinicios <vinicioscsoares@gmail.com>
2024-05-26 09:55:04 -0300

    Project Init

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <main>
+          SIDEBAR
+          {children}
+        </main>
+      </html>
+    );
+  }
